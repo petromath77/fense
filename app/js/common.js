366c2eb959e2fe315196688d46c8d7462f4549f7
petromath77 <petromath77@gmail.com>
2017-12-13 17:03:44 +0300

    team&spam&testims_sections

--- app/js/common.js
+++ app/js/common.js
@@ -5,5 +5,11 @@ $(function() {
 	  itemSelector: '.grid-item',
 	  columnWidth: 0
 	});
-
+	$('.value-percent').each(function() {
+	  var dataValue = $(this).data('inner');
+	  var dataCount = $(this).html("<span class='data_inner'>"+dataValue+"</span>");
+	    $(this).animate({
+	        width: dataValue
+	    });
+	});
 });
